e876661f4355eb4e762e0a11c56c435c5644edd2
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-06-11 16:59:46 +0530

    update the server

--- server/server.js
+++ server/server.js
@@ -2,23 +2,75 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import codeRoutes from "./routes/codeRoutes.js";
 
+// Routes
+import codeRoutes from "./routes/codeRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+// Config
 dotenv.config();
 
 const app = express();
-// put the routes
+
+// ====================================
+// MIDDLEWARE
+// ====================================
 app.use(cors());
+
 app.use(express.json());
 
+// ====================================
+// ROUTES
+// ====================================
 app.use("/api/code", codeRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+// ====================================
+// TEST ROUTE
+// ====================================
+app.get("/", (req, res) => {
 
+  res.send(
+    "Backend Working Successfully"
+  );
+
+});
+
+// ====================================
+// MONGODB CONNECTION
+// ====================================
+mongoose.connect(
+  process.env.MONGO_URI
+)
+
+.then(() => {
+
+  console.log(
+    "MongoDB Connected"
+  );
+
+})
+
+.catch((err) => {
+
+  console.log(err);
+
+});
+
+// ====================================
+// PORT
+// ====================================
+const PORT =
+  process.env.PORT || 5000;
+
+// ====================================
+// START SERVER
+// ====================================
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
 });
